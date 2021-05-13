8d77d3104bf731ebf98d3abf58846376c473c001
gd744 <gonzalezdavid7761@gmail.com>
2021-05-13 12:27:00 -0700

    structured

---
+++ lab6.py
@@ -0,0 +1,11 @@
+"""
+Generating random numbers between 1 qand 20
+based on random number, we will classify by range
+>15 = cherries
+>5 = plum
+>2 = melon
+>1 Bell
+if none, = bar
+Will loop 3 times
+"""
+
