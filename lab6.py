daa28322e13f8a55928dc5e6cc8c7e4e4a24108e
gd744 <gonzalezdavid7761@gmail.com>
2021-05-13 12:28:45 -0700

    psuedocode

--- lab6.py
+++ lab6.py
@@ -9,3 +9,23 @@ if none, = bar
 Will loop 3 times
 """
 
+"""
+import random
+num = generate random number 
+
+    greater than 15, 
+    then the result will be "Cherries"
+    if num is > 10, 
+    then the result will be "Orange"
+    if num is > 5, 
+    then the result will be "Plum"
+    if num is > 2, 
+    then the result will be "Melon"
+    if num is > 1, 
+    then the result will be "Bell"
+    if none
+    result is BAr
+
+    loop three times
+    print the output 
+"""
